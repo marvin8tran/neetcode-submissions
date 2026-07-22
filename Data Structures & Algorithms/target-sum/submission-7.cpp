227e8ada833ba0ae9e44f2a781fdb26e87a61cdc
marvin8tran <marvin8tran@gmail.com>
2026-07-21 19:08:02 -0700

    Add: target-sum - submission-7

---
+++ Data Structures & Algorithms/target-sum/submission-7.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        unordered_map<int, int> dp;
+        //dp[sum] = # of ways to get there
+        dp[0] = 1;
+
+        for(int n : nums){
+            unordered_map<int, int> next;
+            for(auto& [val, path] : dp){
+                next[val + n] += path;
+                next[val - n] += path;
+            }
+            dp = next;
+        }
+        return dp[target];
+    }
+};
