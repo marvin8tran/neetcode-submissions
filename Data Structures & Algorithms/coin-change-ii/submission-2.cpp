b0b4d78d43387c5ccd8fbb1189f039f590b4983d
marvin8tran <marvin8tran@gmail.com>
2026-07-26 14:31:39 -0700

    Add: coin-change-ii - submission-2

---
+++ Data Structures & Algorithms/coin-change-ii/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount+1, 0);
+        dp[0] = 1;
+        //amount, # of ways
+
+        for(int n : coins){
+            for(int i = 1; i <= amount; i++){
+                if(i >= n){
+                    dp[i] += dp[i-n];
+                }
+            }
+        }
+        return dp[amount];
+    }
+};
