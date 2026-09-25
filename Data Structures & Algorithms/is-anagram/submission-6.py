9de02dad435c62025673e36aee8be852c99afd19
marvin8tran <marvin8tran@gmail.com>
2026-09-25 09:41:15 -0700

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        seen = [0] * 26
+
+        for i in range(len(s)):
+            seen[ord(s[i]) - ord('a')] += 1
+            seen[ord(t[i]) - ord('a')] -= 1
+        
+        for j in range(26):
+            if seen[j] != 0:
+                return False
+        return True
+        
